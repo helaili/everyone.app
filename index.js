ed11ddb132b70e714349e219e6143e2f98d8b382
Alain Hélaïli <alain@carbonhub.io>
2020-12-15 22:48:52 +0100

    Implementing the team creation

--- index.js
+++ index.js
@@ -2,20 +2,67 @@
  * This is the main entrypoint to your Probot app
  * @param { {app: import('probot').Application} } app
  */
-module.exports = ({ app }) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+module.exports = ({ app }) =>  {
+  app.log.info(`Everyone.app has launched in the ${process.env.NODE_ENV } environment`)
+  let per_page = 100
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'ci') {
+    per_page = 5
+  } 
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+  app.on(['installation.created', 'installation.unsuspend'], async context => {
+    const org = context.payload.installation.account.login
+    
+    context.log.debug({
+      event: context.name,
+      action: context.payload.action,
+      org: org
+    })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    return createTeam(context, org, 'everyone').then( ({data: team}) => {
+      context.log.debug({
+        team: team
+      })
+      return populateTeam(context, org, team, 1, per_page)
+    }).catch(error => {
+      context.log.error({
+        org: org,
+        message: error.message
+      })
+    })
+  })
+}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+async function createTeam(context, org, name) {
+  return context.octokit.teams.create({
+    org: org,
+    name: name
+  })
+}
+
+async function populateTeam(context, org, team, page = 1, per_page = 100) {
+  const {data: members} = await context.octokit.orgs.listMembers({
+    org: org, 
+    per_page: per_page,
+    page: page
+  })
+  
+  context.log.debug({
+    org: org, 
+    team: team, 
+    page: page, 
+    per_page: per_page,
+    members: members
+  })
+
+  if (members.length == per_page) {
+    populateTeam(context, org, team, page+1, per_page)
+  }
+
+  for (const member of members) {
+    context.octokit.teams.addOrUpdateMembershipForUserInOrg({
+      org: org,
+      team_slug: team.slug,
+      username: member.login
+    })
+  } 
+}
